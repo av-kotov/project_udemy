88d8f2783523f530305cb9274826a4e1318b583a
Alex <example@gmail.com>
2021-03-27 01:30:18 +0300

    practice - films part2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 "use strict";
 
 
-let a = +prompt("Введите первое число", "");
-let b = +prompt("Введите второе число", "");
+if(4 == 9) {
+  console.log('OK!');
+} else {
+  console.log('error!');
+}
 
-alert( a + b );
+
+
+// if (num < 49) {
+//   console.log('Мало!');
+// } else if (num > 100) {
+//   console.log('Много!');
+// } else {
+//   console.log('OK!');
+// }
+
+// (num === 50) ?  console.log('OK!') : console.log('error!');
+
+const num = 50;
+
+switch (num) {
+  case 49:
+    console.log('error!');
+    break;
+  case 100:
+    console.log('error!');
+    break;
+  case 50:
+    console.log('OK!');
+    break;
+  default:
+    console.log('F**ck');
+    break;
+}
--- practice_step_2/script.js
+++ practice_step_2/script.js
@@ -21,23 +21,61 @@ if (login == "admin") {
 
 // homework les 12
 
+// while ( true )
+// {
+//     var variable1 = prompt ('Write "2", please');
+
+//     if ( variable1 == 2 ) break;
+
+//     alert( "Error. Invalid value" );
+// }
+
 const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
- };
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+if (numberOfFilms == null || numberOfFilms == '') {
+  +prompt ('Сколько фильмов вы уже посмотрели?', '');
+} else {
+  if (personalMovieDB.count < 10) {
+  alert('мало!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+  alert('classic');
+} else if(personalMovieDB.count > 30) {
+  alert('Good!');
+  } else {
+    alert ('OK!');
+  }
+}
 
 
- const a = prompt ('Один из последних просмотренных фильмов:?', ''),
-       b = prompt ('На сколько оцените его?', ''),
-       c = prompt ('Один из последних просмотренных фильмов:?', ''),
-       d = prompt ('На сколько оцените его?', '');
 
- personalMovieDB.movies[a] = b;
- personalMovieDB.movies[c] = d;
 
- console.log(personalMovieDB)
+
+
+
+
+
+
+
+//  const a = prompt ('Один из последних просмотренных фильмов:?', ''),
+//        b = prompt ('На сколько оцените его?', ''),
+//        c = prompt ('Один из последних просмотренных фильмов:?', ''),
+//        d = prompt ('На сколько оцените его?', '');
+
+//  personalMovieDB.movies[a] = b;
+//  personalMovieDB.movies[c] = d;
+
+//  console.log(personalMovieDB)
+
+// for (let i = 2; i <= 10; ++i) {
+//   if (i % 2 == 0) {
+//     alert(i);
+//   }
+// }
