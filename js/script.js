79705186d1cb6e54a2ad3bdd3fa27d9b060139cd
Alex <example@gmail.com>
2021-04-05 02:14:16 +0300

    practice object

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 "use strict";
 
 
-function showFirstMessage(text) {
-  console.log(text);
-}
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  },
+  makeTest: function() {
+    console.log("Test");
+  }
+};
 
-showFirstMessage('Hello, world');
+options.makeTest();
 
-const calc = (a, b) => a + b;
+const {border, bg} = options.colors;
 
+console.log(border);
+// console.log(Object.keys(options).length);
 
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof(options[key]) === 'object') {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//   }
+// }
+// console.log(counter);
