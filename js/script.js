1e7dd035e1beeee36948aac9c88563e2cbe721ed
Alex <example@gmail.com>
2021-04-06 00:11:07 +0300

    practice object

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 "use strict";
 
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
-  },
-  makeTest: function() {
-    console.log("Test");
-  }
-};
+// const options = {
+//   name: 'test',
+//   width: 1024,
+//   height: 1024,
+//   colors: {
+//     border: 'black',
+//     bg: 'red'
+//   },
+//   makeTest: function() {
+//     console.log("Test");
+//   }
+// };
 
-options.makeTest();
+// options.makeTest();
 
-const {border, bg} = options.colors;
+// const {border, bg} = options.colors;
 
-console.log(border);
+// console.log(border);
 // console.log(Object.keys(options).length);
 
 // let counter = 0;
@@ -33,3 +33,48 @@ console.log(border);
 //   }
 // }
 // console.log(counter);
+
+
+// filter
+
+const filterThis = [2, 4, 3, 6, 8, 9];
+
+const filter = (arr, fn) => {
+   const filteredArr = [];
+   for (let elem of arr)  {
+     if (fn(elem)) filteredArr.push(elem);
+   }
+   return filteredArr;
+};
+
+console.log(filter(filterThis, (elem) => elem % 3 ==0));
+
+
+
+
+
+//some
+const someOfThis = ['Java', 'JavaScript', 'Python'];
+
+const some = (arr, fn) => {
+  for (let elem of arr)  {
+    if (fn(elem)) return true;
+  }
+  return false;
+};
+
+console.log(some(someOfThis, (elem) => elem === 'JavaS'));
+
+
+//map
+const mapMe = [1000, 800, 2400, 500];
+
+const map = (arr, fn) => {
+  const mappedArr = [];
+  for( let elem of arr) {
+    mappedArr.push(fn(elem));
+  }
+  return mappedArr;
+};
+
+console.log(map(mapMe, (elem) => elem + 200));
