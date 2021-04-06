a493330dd4bdbc877ec321ad3434260941a1dd1e
Alex <example@gmail.com>
2021-04-07 01:42:32 +0300

    practice object

--- practice_step_2/script.js
+++ practice_step_2/script.js
@@ -116,3 +116,21 @@ const personalMovieDB = {
  }
 
  writeYourGenres();
+
+
+
+ //// массивы
+
+ const arr = [1, 2, 3, 6, 8];
+
+
+
+//  console.log(arr);
+
+//  for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+//  };
+
+arr.forEach(function(item, i, arr)  {
+  console.log(`${i}: ${item} внутри массива ${arr}`);
+});
